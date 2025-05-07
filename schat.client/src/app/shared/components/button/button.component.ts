2084373e548da9f844681369160aa424e87cc18e
Hauja47 <hoangletrung.hau01@gmail.com>
2025-05-07 17:19:59 +0700

    Add button and text input shared component

---
+++ schat.client/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,10 @@
+import {Component, input} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+}

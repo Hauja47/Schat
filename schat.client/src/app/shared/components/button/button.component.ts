ffb2ec77e9b7676db1560e602bb596f1e1e01bb7
Hauja47 <hoangletrung.hau01@gmail.com>
2025-05-30 17:04:08 +0700

    update button, add css for disabled button, add button's types

--- schat.client/src/app/shared/components/button/button.component.ts
+++ schat.client/src/app/shared/components/button/button.component.ts
@@ -1,5 +1,7 @@
 import {Component, input} from '@angular/core';
 
+export type ButtonType = "button" | "submit" | "reset";
+
 @Component({
   selector: 'app-button',
   imports: [],
@@ -7,4 +9,6 @@ import {Component, input} from '@angular/core';
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+  disabled = input<boolean>(false);
+  type =  input<ButtonType>('button');
 }

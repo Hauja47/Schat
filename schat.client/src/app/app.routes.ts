838eeb9ebab2119ea948b2d938b8f46af0e49120
Hauja47 <hoangletrung.hau01@gmail.com>
2025-05-30 13:58:27 +0700

    update before upgrade to angular v20

--- schat.client/src/app/app.routes.ts
+++ schat.client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {SignInComponent} from './features/auth/pages/sign-in/sign-in.component';
 import {SignUpComponent} from './features/auth/pages/sign-up/sign-up.component';
+import {ResetPasswordComponent} from './features/auth/pages/reset-password/reset-password.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,11 @@ export const routes: Routes = [
         title: "Register",
         path: 'sign-up',
         component: SignUpComponent
+      },
+      {
+        title: "Reset Password",
+        path: 'reset-password',
+        component: ResetPasswordComponent,
       }
     ]
   },

cff42d74b7112505d66bb2f49a18e0b1574c78bd
Hauja47 <hoangletrunghau01@gmail.com>
2025-06-02 16:44:36 +0700

    add capitalize-first-letter.ts and index for utils

---
+++ schat.client/src/app/core/utils/index.ts
@@ -0,0 +1,9 @@
+import {toError} from './to-error';
+import {capitalizeFirstLetter} from './capitalize-first-letter';
+import {getControlName} from './get-control-name';
+
+export {
+  toError,
+  capitalizeFirstLetter,
+  getControlName
+}

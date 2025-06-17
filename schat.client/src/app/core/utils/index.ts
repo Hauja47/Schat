f5a15f99bfe9c508ac113c5fed84075d6c96f05d
Hauja47 <hoangletrunghau01@gmail.com>
2025-06-17 17:33:26 +0700

    Update to using lodash

--- schat.client/src/app/core/utils/index.ts
+++ schat.client/src/app/core/utils/index.ts
@@ -1,9 +1,7 @@
 import {toError} from './to-error';
-import {capitalizeFirstLetter} from './capitalize-first-letter';
 import {getControlName} from './get-control-name';
 
 export {
   toError,
-  capitalizeFirstLetter,
   getControlName
 }

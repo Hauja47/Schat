ae0cfa13837ae9ee06848e48ef05ae4b30d5a316
Hauja47 <hoangletrung.hau01@gmail.com>
2025-06-17 17:35:01 +0700

    Update sign-up

--- schat.client/src/app/features/auth/pages/sign-up/sign-up.component.ts
+++ schat.client/src/app/features/auth/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
 import {LayoutComponent} from '../../components/layout/layout.component';
 import {TextInputBoxComponent} from '../../../../shared/components/text-input-box/text-input-box.component';
 import {ErrorsListComponent} from '../../../../shared/components/errors-list/error-list.component';
@@ -7,6 +7,9 @@ import {ButtonComponent} from '../../../../shared/components/button/button.compo
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {OrSeparatorComponent} from '../../components/or-separator/or-separator.component';
 import {OauthBtnGroupComponent} from '../../components/oauth-btn-group/oauth-btn-group.component';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {getControlName, toError} from '../../../../core/utils';
+import * as fp from 'lodash/fp';
 
 @Component({
   selector: 'app-sign-up',
@@ -18,14 +21,73 @@ import {OauthBtnGroupComponent} from '../../components/oauth-btn-group/oauth-btn
     RouterLink,
     RouterLinkActive,
     OrSeparatorComponent,
-    OauthBtnGroupComponent
+    OauthBtnGroupComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  emailErrors: Errors | undefined;
-  usernameErrors: Errors | undefined;
-  confirmPasswordErrors: Errors | undefined;
-  passwordErrors: Errors | undefined;
+  emailErrors = signal<Errors | undefined>({
+    errors: {}
+  });
+  usernameErrors = signal<Errors | undefined>({
+    errors: {}
+  });
+  passwordErrors = signal<Errors | undefined>({
+    errors: {}
+  });
+  confirmPasswordErrors = signal<Errors | undefined>({
+    errors: {}
+  });
+
+  signUpForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  })
+
+  /* region Get FormControls */
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get username() {
+    return this.signUpForm.get('username');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword');
+  }
+
+  /* endregion */
+
+  onEmailInput() {
+    this.setErrors(this.email!, this.emailErrors);
+  }
+
+  onUsernameInput() {
+    this.setErrors(this.username!, this.usernameErrors);
+  }
+
+  onPasswordInput() {
+    this.setErrors(this.password!, this.passwordErrors);
+  }
+
+  onConfirmPasswordInput() {
+    this.setErrors(this.confirmPassword!, this.confirmPasswordErrors);
+  }
+
+  setErrors(
+    control: AbstractControl,
+    errors: WritableSignal<Errors | undefined>,
+  ) {
+    const controlName = fp.startCase(getControlName(control)!);
+    errors.set(toError(control.errors, controlName))
+  }
 }
